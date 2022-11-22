09af01f52abed26118c04ded746831391ab4c390
Jacob <j31c0@hotmail.com>
2022-11-22 11:29:15 -0500

    Solve reverseString problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let myString = string.split("");
+    let reverseString="";
+    for(let i=string.length-1 ; i>=0 ; i--) {
+        /* console.log(myString[i]); // Print the string in an array*/ 
+        /* reverseString.push(myString[i]); Print reverse string in an array*/
+        reverseString += myString[i]; // Concatenate reverse string in 'myNewString'
+    }
+    return reverseString;
 };
-
+console.log(reverseString("Hello, World!"));
 // Do not edit below this line
 module.exports = reverseString;
