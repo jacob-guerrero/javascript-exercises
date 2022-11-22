42612599d94131f17f2cba9d3f4442b8852d9a4c
Jacob <j31c0@hotmail.com>
2022-11-22 10:31:00 -0500

    Solve exercise number two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i=0; i < num; i++) {
+        concatString += string;
+    }
+    return concatString;
 };
-
+console.log(repeatString("hey",10));
 // Do not edit below this line
 module.exports = repeatString;
