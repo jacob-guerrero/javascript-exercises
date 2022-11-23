709352d680af699810e8f30722478a06da5b2641
Jacob <j31c0@hotmail.com>
2022-11-22 22:43:23 -0500

    Add index finder

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let itemIndex;
 
+    itemIndex = array.indexOf(remove[0]);
+    array.splice(itemIndex, 1);
+    
+    return array;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3, 4))
 // Do not edit below this line
 module.exports = removeFromArray;
