deecf3357cd72583428215cc4e7e8cd76490b52b
Jacob <j31c0@hotmail.com>
2022-11-24 01:32:27 -0500

    Add help condition

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -7,9 +7,13 @@ const removeFromArray = function(array, ...remove) {
     array.splice(itemIndex, 1);
     i++;
     }
+    if (array.length == 2) {
+        itemIndex = array.indexOf(remove[i]);
+        array.splice(0, 1);
+    }
     
     return array;
 };
-console.log(removeFromArray([1, 2, 3, 4], 3, 4))
+console.log(removeFromArray([1, 2, 3, 4], 3, 1, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
