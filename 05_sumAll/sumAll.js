23e629ab00abab37046af99783d466f151eed4a1
Jacob <j31c0@hotmail.com>
2022-11-28 20:12:04 -0500

    Add solution w/ validation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let limInf= 0;
+    let limSup= 0;
+    if ((firstNum || secondNum) < 0 ||
+        typeof firstNum !== 'number' ||
+        typeof secondNum !== 'number' ) {
+        return 'ERROR';
+    }
+    if (firstNum <= secondNum) {
+        limInf = firstNum;
+        limSup = secondNum;
+    } else {
+        limInf = secondNum;
+        limSup = firstNum;
+    }
+    let sum = limInf;
+    let result = 0;
+    for (let i=0; i<limSup; i ++) {
+        sum = limInf + i;
+        result = result + sum;
+    }
+    return result;
 };
+
+console.log(sumAll(10, "1"));
 
 // Do not edit below this line
 module.exports = sumAll;
