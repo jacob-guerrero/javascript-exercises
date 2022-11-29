7093a8c2435d9d70732639df372dbf186bd9916f
Jacob <j31c0@hotmail.com>
2022-11-29 16:15:16 -0500

    Add functions to convert and round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let far = (num - 32) * 5/9;
+  return Math.round(far * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let cel = (num * 9/5 + 32);
+  return Math.round(cel * 10) / 10;
 };
+
+console.log(convertToFahrenheit(4));
+console.log(convertToCelsius(52));
 
 // Do not edit below this line
 module.exports = {
