c8b90798348c795cb2ebfa3844f9ad5fb8b09378
Jacob <j31c0@hotmail.com>
2022-12-26 17:47:56 -0500

    Add solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	/* for (let i=0; i < array.length ; i++) {
+    sum += array[i];
+  } */
+  array.forEach(elem => {
+    sum += elem;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let mult = 1;
+  arr.forEach(elem => {
+    mult *= elem;
+  });
+  return mult
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let res = 1;
+  for (let i=1; i<=n; i++) {
+    res *= i;
+  }
+	return res
 };
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
