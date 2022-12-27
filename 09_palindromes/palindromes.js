f4694a8b10b0b8ca4ea9f2d9eccc63e5393b6baa
Jacob <j31c0@hotmail.com>
2022-12-26 23:01:18 -0500

    Add solution to palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let letters = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let myArr = letters.split('');
+    /* let arr = Array.from(str); */ //This is a slow method
+    let reverseArr = myArr.reverse();
+    let reverseStr = reverseArr.join('')
 
+    return reverseStr === letters
 };
-
+console.log(palindromes('racecar!'))
 // Do not edit below this line
 module.exports = palindromes;
